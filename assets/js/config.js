329da10cfed5a009ed395672bc15cb27bf811a6f
hadiajami <58671769+hadiajami@users.noreply.github.com>
2026-09-09 18:05:49 +0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,7 +2,7 @@
 // 2) Replace these two values from Supabase > Project Settings > API.
 // Public anon keys are designed to be used in browser apps.
 window.HADI_CONFIG = {
-  SUPABASE_URL: "https://hrkyzgaeqxbsxpujyycm.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://hrkyzgaeqxbsxpujyycm.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhya3l6Z2FlcXhic3hwdWp5eWNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg5NjEzNDksImV4cCI6MjEwNDUzNzM0OX0.iV_3oBso1FsXPE6dauLuY0hFfizQVJeckRPTx3utcqs",
   WHATSAPP_NUMBER: "96176150404",
   CURRENCY: "USD"
